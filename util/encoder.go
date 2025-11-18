90c78b87a0cff667777f9c95e41d4ad86d6f5400
Kirill Zakusov <username918r818@outlook.com>
2025-11-18 18:49:25 +0300

    feat: add URL encoding function and corresponding tests

---
+++ util/encoder.go
@@ -0,0 +1,28 @@
+package util
+
+import "fmt"
+
+func EncodeUrl(b []byte) string {
+	allowed := func(c byte) bool {
+		if c >= '0' && c <= '9' { return true }
+		if c >= 'A' && c <= 'Z' { return true }
+		if c >= 'a' && c <= 'z' { return true }
+		switch c {
+		case '.', '-', '_', '~':
+			return true
+		}
+		return false
+	}
+
+	out := make([]byte, 0, len(b)*3)
+
+	for _, c := range b {
+		if allowed(c) {
+			out = append(out, c)
+		} else {
+			out = append(out, fmt.Sprintf("%%%02X", c)...)
+		}
+	}
+
+	return string(out)
+}
---
+++ util/encoder_test.go
@@ -0,0 +1,21 @@
+package util_test
+
+import (
+	"github.com/username918r818/torrent-client/util"
+	"testing"
+)
+
+func TestEncoding(t *testing.T) {
+	data := []byte{
+		0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf1,
+		0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x12,
+		0x34, 0x56, 0x78, 0x9a,
+	}
+
+	encodedUrl := util.EncodeUrl(data)
+	expected := "%124Vx%9A%BC%DE%F1%23Eg%89%AB%CD%EF%124Vx%9A"
+
+	if encodedUrl != expected {
+		t.Errorf("expected:\n%v\ngot:\n%v", expected, encodedUrl)
+	}
+}
