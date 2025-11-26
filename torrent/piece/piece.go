156172226b8c9f7c3ad452f140fd30059fabb82f
Kirill Zakusov <username918r818@outlook.com>
2025-11-26 17:45:49 +0300

    feat: some piece logic

---
+++ torrent/piece/piece.go
@@ -0,0 +1,72 @@
+package piece
+
+import (
+	"crypto/sha1"
+	"sync"
+)
+
+type PieceState int
+
+const (
+	NotStarted PieceState = iota
+	InProgress
+	Downloaded
+	Validated
+	Saved
+	Deleted
+)
+
+type Piece interface {
+	GetState() PieceState
+	SetState(PieceState)
+}
+
+type rawPiece struct {
+	sync.RWMutex
+	state PieceState
+}
+
+func (r *rawPiece) GetState() PieceState {
+	return r.state
+}
+
+func (r *rawPiece) SetState(s PieceState) {
+	r.state = s
+}
+
+type rawPieceWithData struct {
+	rawPiece
+	Hash [20]byte
+	Data []byte
+}
+
+type rawPieceWithDataAndBlockMap struct {
+	rawPieceWithData
+	rbt any
+}
+
+type PieceNotStarted = rawPiece
+type PieceInProgress = rawPieceWithData
+type PieceDownloaded = rawPieceWithData
+type PieceValidated = rawPieceWithDataAndBlockMap
+type PieceSaved = rawPiece
+
+type PieceArray struct {
+	sync.RWMutex
+	length [5]int64
+	pieces []Piece
+}
+
+func Validate(data []byte, hash [20]byte) bool {
+	return sha1.Sum(data) == hash
+}
+
+func InitPieceArray(totalBytes, pieceLength int64) (a PieceArray) {
+	a.length[NotStarted] = totalBytes
+	arrLength := totalBytes / pieceLength
+	if totalBytes%pieceLength > 0 {
+		arrLength++
+	}
+	a.pieces = make([]Piece, arrLength)
+	return
+}
---
+++ torrent/piece/piece_test.go
@@ -0,0 +1,47 @@
+package piece_test
+
+import (
+	"encoding/hex"
+	"strings"
+	"testing"
+
+	"github.com/username918r818/torrent-client/torrent/piece"
+)
+
+func TestPieceValidation(t *testing.T) {
+	t.Run("successful validating", func(t *testing.T) {
+		data := []byte("This page intentionally left blank.")
+
+		hexStr := "af 06 49 23 bb f2 30 15 96 aa c4 c2 73 ba 32 17 8e bc 4a 96"
+		hexStr = strings.ReplaceAll(hexStr, " ", "")
+		hashS, err := hex.DecodeString(hexStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var hash [20]byte
+		copy(hash[:], hashS)
+
+		if !piece.Validate(data, hash) {
+			t.Errorf("expected validation to succeed, but it failed")
+		}
+	})
+
+	t.Run("failed validating", func(t *testing.T) {
+		data := []byte("This page intentionally right blank.")
+
+		hexStr := "af 06 49 23 bb f2 30 15 96 aa c4 c2 73 ba 32 17 8e bc 4a 96"
+		hexStr = strings.ReplaceAll(hexStr, " ", "")
+		hashS, err := hex.DecodeString(hexStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		var hash [20]byte
+		copy(hash[:], hashS)
+
+		if piece.Validate(data, hash) {
+			t.Errorf("expected validation to fail, but it succeeded")
+		}
+	})
+}
