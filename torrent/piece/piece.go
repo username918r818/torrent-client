9698a4e474a3640e9980dd6cf5d7ec8100a24aea
Kirill Zakusov <username918r818@outlook.com>
2025-12-01 20:38:09 +0300

    refactor: restructure pieceSession and add file management tasks

--- torrent/piece/piece.go
+++ torrent/piece/piece.go
@@ -73,24 +73,27 @@ const (
 type pieceSession struct {
 	listDlock  sync.Mutex // lock for downloaded
 	listTSLock sync.Mutex // lock for toSave
-	ListSLock  sync.Mutex // lock for saving
-	ListSDLock sync.Mutex // lock for saved
+	listSLock  sync.Mutex // lock for saving
+	listSDLock sync.Mutex // lock for saved
 
-	downloaded, toSave, saving, saved *util.RangeSet
+	downloaded, toSave, saving, saved util.RangeSet
 
-	pieceMutex   sync.Mutex         // lock for pieceMutexes
-	pieceMutexes map[int]sync.Mutex // need pieceMutex be acquired for use
+	pieceMutex   sync.RWMutex       // lock for pieceMutexes
+	pieceMutexes map[int]sync.Mutex // need pieceMutex be acquired for use // TODO check if need
 	pieces       map[int][]byte
+
+	pieceLength int64
 
 	readyFileWorkers atomic.Int64
 
 	fileMutex       sync.Mutex
-	rangeFile       util.RangeSet
+	fileRanges      util.RangeSet
+	rangeFile       map[util.Range]string
 	fileRange       map[string]util.Range
 	filePath        map[string][]string // clear after alloc
 	filePointer     map[string]*os.File
-	filesToAllocate map[int]struct{}
-	filesToDelete   map[int]toDeleteEnum
+	filesToAllocate map[string]struct{}
+	filesToDelete   map[string]toDeleteEnum
 	ch              Channels
 	cb              cbChannels
 }
@@ -105,7 +108,9 @@ type Channels struct {
 	Report      chan<- StatDiff
 	ToCreate    <-chan map[string]util.Range
 	ToDelete    <-chan map[string]util.Range
-	ToDownload  <-chan []int
+	ToDownload  chan<- []int
+	NewFile     <-chan NewFileTask
+	DeleteFile  <-chan DeleteFileTask
 	saved       <-chan file.WReport
 	deleted     <-chan file.DReport
 	allocated   <-chan file.AReport
@@ -117,11 +122,21 @@ type cbChannels struct {
 	allocated chan<- file.AReport
 }
 
+type NewFileTask struct {
+	Path           []string
+	Offset, Length int64
+}
+
+type DeleteFileTask struct {
+	SlicePath  []string
+	StringPath string
+}
+
 func Validate(data []byte, hash [20]byte) bool {
 	return sha1.Sum(data) == hash
 }
 
-func Init(ctx context.Context, amount int, ch Channels) {
+func Init(ctx context.Context, amount int, ch Channels, pieceLength int64) {
 	ps := pieceSession{}
 	sc := make(chan file.WReport)
 	dc := make(chan file.DReport)
@@ -132,6 +147,161 @@ func Init(ctx context.Context, amount int, ch Channels) {
 	ps.cb.saved = sc
 	ps.cb.deleted = dc
 	ps.cb.allocated = ac
+
+	ps.pieceLength = pieceLength
+
+	helperReportCh := make(chan StatDiff)
+	helperToDownloadCh := make(chan []int)
+
+	go helperStats(ctx, helperReportCh, ch.Report)
+	go helperToDownload(ctx, helperToDownloadCh, ch.ToDownload)
+
+	for range amount {
+		go ps.start(ctx)
+	}
+}
+
+// TODO
+func (ps *pieceSession) start(ctx context.Context) {}
+
+// sends to file worker, removes from toSave, adds to saving
+// need to be locked fileRange, osFile
+func (ps *pieceSession) save(r util.Range, fs string) {
+	fr := ps.fileRange[fs]
+	osFile := ps.filePointer[fs]
+	task := file.WTask{Callback: ps.cb.saved, Id: r, File: osFile}
+
+	task.Offset = r.First - fr.First
+	task.Length = r.Second - r.First
+	task.Data = make([]byte, task.Length)
+
+	ps.pieceMutex.RLock()
+	curPiece := r.First / ps.pieceLength
+	curIndex := r.First % ps.pieceLength
+	dataIndex := int64(0)
+
+	for dataIndex < task.Length {
+		copy(task.Data[dataIndex:], ps.pieces[int(curPiece)][curIndex:])
+		dataIndex += ps.pieceLength - curIndex
+		curIndex = 0
+		curPiece++
+	}
+
+	ps.ch.Save <- task
+}
+
+// sets range saved and remove Downloaded and saving, checks if need to delete
+func (ps *pieceSession) setSaved(report file.WReport) {
+	r, ok := report.Id.(util.Range)
+	if !ok {
+		panic("piece worker: file.WReport.Id: wrong type ")
+	}
+	ps.listDlock.Lock()
+	ps.listSLock.Lock()
+	ps.listSDLock.Lock()
+	ps.downloaded.Extract(r)
+	ps.saving.Extract(r)
+	ps.saved.Insert(r)
+
+	curPiece := r.First / ps.pieceLength
+	if r.First%ps.pieceLength != 0 {
+		curPiece++
+	}
+
+	ps.pieceMutex.Lock()
+
+	for curPiece*ps.pieceLength <= r.Second {
+		delete(ps.pieces, int(curPiece))
+		curPiece++
+	}
+	ps.pieceMutex.Unlock()
+
+	ps.fileMutex.Lock()
+
+	// it should return file range
+	fr := ps.fileRanges.FindIntersections(r)
+	if len(fr) == 0 {
+		panic("piece: setSeved(): len(fr) == 0")
+	}
+	f, ok := ps.rangeFile[fr[0]]
+	if !ok {
+		panic("piece: setSeved(): file not found")
+	}
+
+	if ps.filesToDelete[f] == toDelete {
+		ps.fileMutex.Unlock()
+		ps.tryToDelete()
+	}
+}
+
+// sets range toSave, e.g. if file write fails or received a new file range to download
+func (ps *pieceSession) setToSave(report file.WReport) {}
+
+// receives index and adds it to Downloaded, also report
+func (ps *pieceSession) validate(index int, piece, hash []byte) bool
+
+// adds file and inserts ranges to toSave range, if deleting, set to restore, else put in queue to allocate, also toDownload(...)
+func (ps *pieceSession) addFile(t NewFileTask) {}
+
+// removes file and remove ranges from toSave range, if file is saving or saved, marks to delete
+func (ps *pieceSession) removeFile(t DeleteFileTask) {}
+
+// try to allocate
+func (ps *pieceSession) tryToAllocate() {}
+
+// allocs file
+func (ps *pieceSession) alloc(f string) {}
+
+// checks if it has ready file workers and filesToDelete not saving and saved and delete(file)
+func (ps *pieceSession) tryToDelete() {}
+
+// if file is not saving and saved then delete (and remove from saved range and set status deleting), else marks file to delete if saving
+func (ps *pieceSession) delete(f string) {}
+
+// checks if it has ready file workers and ranges and file is allocated then save(range)
+func (ps *pieceSession) tryToSave()
+
+// sends indicies of pieces to download
+func (ps *pieceSession) toDownload(toDownload []int)
+
+// creates helper stats foroutine
+func helperStats(ctx context.Context, in <-chan StatDiff, out chan<- StatDiff) {
+	cur := StatDiff{}
+	for {
+		select {
+		case new := <-in:
+			cur.ToDownload += new.ToDownload
+			cur.Validated += new.Validated
+			cur.Saving += new.Saving
+			cur.Saved += new.Saved
+		case out <- cur:
+			cur = StatDiff{}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+// creates helper toDownload foroutine
+func helperToDownload(ctx context.Context, in <-chan []int, out chan<- []int) {
+	cur := make([]int, 0)
+	unique := make(map[int]struct{})
+	for {
+		select {
+		case new := <-in:
+			for _, v := range new {
+				if _, ok := unique[v]; !ok {
+					cur = append(cur, v)
+					unique[v] = struct{}{}
+				}
+			}
+		case out <- cur:
+			cur = make([]int, 0)
+			unique = make(map[int]struct{})
+		case <-ctx.Done():
+			return
+		}
+	}
 }
 
 // func InitPieceArray(totalBytes, pieceLength int64) (a PieceArray) {
